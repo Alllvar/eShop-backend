4f7d6f14046424c57d4ecf90829dcee01d16fbce
viktor <tkvbifua@gmail.com.com>
2021-01-28 21:27:08 +0200

    added router for reviews

--- server/router.ts
+++ server/router.ts
@@ -53,7 +53,8 @@ router.get('/products/:id', async (request: Request, response: Response) => {
 
 router.get('/products/:id/reviews', async (request: Request, response: Response) => {
     const query: MongoQuery<IReview> = { productId: new mongoose.mongo.ObjectId(request.params.id) };
-    const result = await Review.findOne(query);
+    const result = await Review.find(query);
+    console.log(result)
 
     return response.json(result);
 });
